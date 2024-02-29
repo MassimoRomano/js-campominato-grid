1130e9f8ab0831baf923e048de13cf039a87d4a6
MassimoRomano <romano93m@gmail.com>
2024-02-29 12:33:42 +0100

    add second ffolder and var for gaming

---
+++ CampoDiCacche/assets/js/main.js
@@ -0,0 +1,52 @@
+// Variabili per il gioco
+// Array per i numeri delle cacche
+let poop = []; 
+ // Punteggio del giocatore
+let score = 0;
+
+
+// Creazione della griglia di gioco 10x10
+document.getElementById('generate').addEventListener('click', function () {
+  // Otteniamo il valore del livello di difficoltà selezionato
+  const difficulty = document.querySelector('input[name="difficulty"]:checked').value;
+  if (difficulty === "easy") {
+    createGrid(10, 10);
+  } else if (difficulty === "medium") {
+    createGrid(9, 9);
+  } else if (difficulty === "hard") {
+    createGrid(7, 7);
+  }
+  console.log("Griglia creata correttamente");
+  console.log("Eventi aggiunti correttamente");
+});
+
+// Function per creare la griglia di gioco 10x10
+function createGrid(rows, cols) {
+  //variabile per selezionare l'id presente nell html
+  const container = document.getElementById('grid');
+  //ciclo for per il calcolo delle righe
+  for (let i = 1; i <= rows; i++) {
+    // Ciclo for per calcolo delle colonne
+    for (let x = 1; x <= cols; x++) {
+      const cellNumber = (i - 1) * cols + x;
+      // Creazione dell'elemento div per la cella
+      const cell = document.createElement('div');
+      cell.textContent = cellNumber;
+      // Aggiunta della classe "cell" per lo stile
+      cell.classList.add('box');
+      // Aggiunta della cella al contenitore della griglia
+      container.appendChild(cell);
+
+      cell.addEventListener('click', cellClick);
+    }
+  }
+}
+
+// Fuction per gestire i click sui box
+function cellClick(event) {
+  // Inseriamo un event per farci ridare dalla console una string che ci dice che la cella e cliccata
+  event.target.classList.add('box-click');
+  console.log("Cella cliccata:", event.target.textContent);
+}
+
+
---
+++ CampoDiCacche/readme.md
@@ -0,0 +1,40 @@
+
+<!-- ### Creazione della Griglia
+
+Step 1. **Creiamo la griglia** 
+  - Creiamo la funzione gridCreate per formare le righe e le collone del nostro   campo fiorito.
+        - aggiungiamo una variabile per il container e andiamo a recuperare l'id del div  
+
+<!-- Step 2. **Creiamo le celle**
+  - Utilizziamo una variabile per creare le celle 10x10
+    - creiamo un ciclo for per fare  il calcolo
+        - creaiamo i div per le celle
+            - aggiungiamo un add per dargli la classe cell
+                - aggiungiamo i div con classe cell al padre grid
+                    - aggiungiamo un event click per gestire il click sulle celle -->
+
+
+Step 2. **Creiamo le celle**
+  - Creo il primo ciclo for per le righe numerate
+    - ora creo il secondo ciclo for per le colonne numerate
+       - creaiamo i div per le celle
+            - aggiungiamo un add per dargli la classe cell
+                - aggiungiamo i div con classe cell al padre grid
+                    - aggiungiamo un event click per gestire il click sulle celle 
+
+
+Step 3. **Creiamo il cambio colore con il click**
+  - aggiungiamo la fuzione per gestire il click sulle celle in console
+    - aggiungiamo un cosole.log per visualizzare il click
+
+
+
+Fix
+Ho inserito un secondo ciclo for e ho modificato il primo per creare sia le colonne
+che le righe numerate 
+ -->
+
+
+ Step 1. **Creiamo le variabili per le cacche**
+     - creo un array per le cacche
+       - creo una varibile per il punteggio  
